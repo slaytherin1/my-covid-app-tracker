8ffc50aab8697c6312d2b0e9e3ffd2681732f998
slaytherin1 <richardbibanco12@gmail.com>
2020-05-04 10:47:11 +0800

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header has-background-dark">
+        <div class="animated zoomIn">
+        <h1 class="title has-text-white">My Corona Virus App</h1>
+        <h2 class="subtitle has-text-white"> - by Richard Bibanco</h2>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+<style scoped>
+h1,h2 {
+    text-align: center;
+}
+</style>
